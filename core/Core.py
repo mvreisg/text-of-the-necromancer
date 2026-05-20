23151a77ce4f00e2aa9dd00225e1c1dd321ce8eb
Marcus Vinicius <marcus_viniciusreis@hotmail.com>
2026-05-20 17:14:55 -0300

    feat: first drawings and movable character

--- core/Core.py
+++ core/Core.py
@@ -1,64 +1,83 @@
-from blessed import Terminal
+from blessed.terminal import Terminal
 
 from core.model.World import World
 
 class Core:
     def __init__(self) -> None:
         self.is_running = True
-        self.x = 0
-        self.y = 0
-        
+        self.row = 0
+        self.column = 0
+
     def run(self) -> None:
         term = Terminal()
 
-        world = World(50, 50)
+        world = World(
+            rows=50, 
+            columns=100
+        )
         world.generate()
 
         with term.fullscreen(), term.cbreak(), term.hidden_cursor():
             while self.is_running:
                 key = term.inkey(timeout=0.016)
 
+                if key == "q":
+                    self.is_running = False
+                    break     
+
                 if key.code == term.KEY_LEFT:
-                    self.x -= 1
+                    world.invalidate()
+                    self.column -= 1
 
                 if key.code == term.KEY_RIGHT:
-                    self.x += 1                    
+                    world.invalidate()
+                    self.column += 1                    
 
                 if key.code == term.KEY_UP:
-                    self.y -= 1
+                    world.invalidate()
+                    self.row -= 1
 
                 if key.code == term.KEY_DOWN:
-                    self.y += 1                                        
+                    world.invalidate()
+                    self.row += 1                                        
 
-                if self.x < 0:
-                    self.x = 0
+                if self.row < 0:
+                    self.row = 0
 
-                if self.x > 49:
-                    self.x = 49
+                if self.column < 0:
+                    self.column = 0
 
-                if self.y < 0:
-                    self.y = 0 
+                if self.column > world.columns - 1:
+                    self.column = world.columns - 1
 
-                if self.y > 49:
-                    self.y = 49                                       
+                if self.row > world.rows - 1:
+                    self.row = world.rows - 1            
 
-                print(
-                    term.home + term.clear, 
-                    end=""
-                )
+                if world.is_valid == False:                    
+                    print(
+                        term.home + term.clear, 
+                        end=""
+                    )
 
-                for x in range(49):
-                    for y in range(49):
-                        print(
-                            term.move_xy(x, y) + term.white("#"),
-                            end="",
-                            flush=True
-                        )   
+                    world_as_string = ""
 
-                print(
-                    term.move_xy(self.x, self.y) + term.green("@"),
-                    end="",
-                    flush=True
-                )
+                    for row in range(world.rows):
+                        for column in range(world.columns):
+                            world_as_string += term.white(world.get_cell(row, column).char)
+                        world_as_string += "\n"
+
+                    print(
+                        world_as_string,
+                        end="",
+                        flush=True
+                    ) 
+
+                    print(
+                        term.move_xy(self.column, self.row) + term.green("@"),
+                        end="",
+                        flush=True
+                    )
+
+                    world.validate()
 
     
--- core/model/World.py
+++ core/model/World.py
@@ -1,19 +1,28 @@
 from core.model.Cell import Cell
 
-
 class World:
     def __init__(
         self, 
-        width: int, 
-        height: int
+        rows: int, 
+        columns: int
     ) -> None:
-        self.width = width
-        self.height = height
+        self.rows = rows
+        self.columns = columns
         self.cells: list[list[Cell]] = []
-    
+        self.is_valid = False
+
     def generate(self):
-        for y in range(self.height):
-            row: list[Cell] = []
-            for x in range(self.width):
-                row.append(Cell(x, y, '#', 'white'))
-            self.cells.append(row)
+        for r in range(self.rows):
+            columns: list[Cell] = []
+            for c in range(self.columns):
+                columns.append(Cell(c, r, '#', 'white'))
+            self.cells.append(columns)
+
+    def invalidate(self):
+        self.is_valid = False     
+    
+    def validate(self):
+        self.is_valid = True
+
+    def get_cell(self, row: int, column: int) -> Cell:
+        return self.cells[row][column]
